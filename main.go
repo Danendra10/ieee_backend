d6cfb5f50ed2c5f057471dc860705c2fa3a6981b
Dan Mac <danendraclever24@gmail.com>
2023-08-29 00:07:03 +0700

    added main.go setup using env

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/danendra10/ieee_backend/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	database.Connect()
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	app := fiber.New()
+	// routes.Setup(app)
+	app.Listen(":" + port)
+	fmt.Println("Running on port " + port)
 }
---
+++ require.sh
@@ -0,0 +1,5 @@
+go get -u github.com/joho/godotenv
+go get -u gorm.io/driver/mysql
+go get -u gorm.io/gorm
+go get -u github.com/joho/godotenv
+go get -u github.com/gofiber/fiber/v2
