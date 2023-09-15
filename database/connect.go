3fcb4242221571a156cc5255790dec1dbde9bcde
Dan Mac <danendraclever24@gmail.com>
2023-09-16 02:35:45 +0700

    using postgree

--- database/connect.go
+++ database/connect.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/danendra10/ieee_backend/models"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres" // Import the PostgreSQL driver
 	"gorm.io/gorm"
 )
 
@@ -18,8 +18,17 @@ func Connect() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	dsn := os.Getenv("DSN")
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// Update the DSN variable to use the PostgreSQL environment variables
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DATABASE"),
+	)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database")
 	}
@@ -41,3 +50,47 @@ func Connect() {
 		}
 	}
 }
+
+// package database
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/danendra10/ieee_backend/models"
+// 	"github.com/joho/godotenv"
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func Connect() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+// 	dsn := os.Getenv("DSN")
+// 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatal("Failed to connect to database")
+// 	}
+
+// 	DB = database
+
+// 	err = DB.Migrator().DropTable(&models.AirPolution{})
+// 	if err != nil {
+// 		log.Fatal("Failed to drop column")
+// 	}
+// 	tables := []interface{}{
+// 		&models.AirPolution{},
+// 	}
+
+// 	for _, table := range tables {
+// 		result := database.AutoMigrate(table)
+// 		if result == nil {
+// 			fmt.Printf("Successfully migrated table: %T\n", table)
+// 		}
+// 	}
+// }
--- go.mod
+++ go.mod
@@ -10,6 +10,9 @@ require (
 	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -21,7 +24,10 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.49.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	gorm.io/driver/mysql v1.5.1 // indirect
+	gorm.io/driver/postgres v1.5.2 // indirect
 	gorm.io/gorm v1.25.4 // indirect
 )
